df1754080f69be4fcf00a871a3eb6e58105c1190
suzaane <your-email@example.com>
2025-05-12 17:03:31 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+    setTimeout(function() {
+        window.location.href = "index.html";  // Replace with your actual main page file name
+    }, 5000);  // Redirect after 3 seconds (3000 milliseconds)
+};
+
+const flowersContainer=document.querySelector('Flowers Container')
